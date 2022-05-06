848c894027e70179f1da08d4b77688f438490dfa
majkelEXE <michal.dubrowski@wp.pl>
2022-05-06 13:58:07 +0200

    websockets

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+const ws = require("ws");
 var express = require("express");
 var app = express();
 const PORT = 3000;
+var path = require("path");
 
-app.get("/", function (req, res) {
-  res.send("dane html odesłane z serwera do przeglądarki");
+app.use(express.static("static"));
+
+const server = app.listen(PORT, function () {
+  console.log("start serwera na porcie " + PORT);
 });
 
-app.listen(PORT, function () {
-  console.log("start serwera na porcie " + PORT);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/static/file1.html"));
+});
+
+const wss = new ws.WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.send("You are connected to server");
+
+  ws.on("message", function message(data, isBinary) {
+    console.log("received: %s", data);
+
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === ws.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
 });
